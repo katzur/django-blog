1551c526e079ed808f446794d6cb448272cc83cc
katzur <zurawska.kasia@gmail.com>
2022-07-31 16:40:52 +0000

    Second deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X-FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['katzur-django-blog.herokuapp.com', 'localhost']
 
